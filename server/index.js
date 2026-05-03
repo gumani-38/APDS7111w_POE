66f6665146a89666a7c67d94c8713058e953c21b
gumani-38 <surprisegumani@gmail.com>
2026-05-03 20:40:11 +0200

    change the rate limiter to two minutes

--- server/index.js
+++ server/index.js
@@ -36,14 +36,12 @@ app.use(
 app.use(cookieParser());
 app.use(express.json({ limit: "30mb" }));
 const limiter = rateLimit({
-  windowMs: 4 * 60 * 1000, // 4 minutes
-  limit: 300, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-  message: "Too many requests from this IP, please try again after 15 minutes",
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 200, // limit each IP to 200 requests per 2 minutes
+  standardHeaders: true, // send combined RateLimit header
+  legacyHeaders: false, // disable X-RateLimit-* headers
+  message: "Too many requests from this IP, please try again later.",
 });
-
 // Apply the rate limiting middleware to all requests.
 app.use(limiter);
 app.use("/api/user", userRoute);
