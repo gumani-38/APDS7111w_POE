a81eb86dcb7c35c9b1bb4247b8b12af329ba6a48
gumani-38 <surprisegumani@gmail.com>
2026-05-03 18:10:53 +0200

    changeed the brute force attact to 60 minutes

--- server/router/user.js
+++ server/router/user.js
@@ -140,7 +140,7 @@ router.get("/logout", AuthorizedUser, async (req, res) => {
 
 // rate limiter for profile route to prevent brute-force attacks
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 60 * 60 * 1000, // 60 minutes
   max: 5, // 5 attempts per window
   message: "Too many login attempts. Please try again later.",
   standardHeaders: true,
